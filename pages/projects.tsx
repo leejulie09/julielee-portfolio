8e6c0e72d6e69cd6e01e3aeb1b2a6defaf7c8e96
Julie Lee <leejulie09@gmail.com>
2023-06-15 17:11:57 +0900

    Modify: text and backgroundcolor

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -71,20 +71,10 @@ export default function Projects() {
             </div>
           </div>
           <span className="menu__item-text">
-            <span className="menu__item-innertext">Impulse /</span>
+            <span className="menu__item-innertext">Educere</span>
           </span>
         </div>
 
-        <div className="menu__item">
-          <div className="menu__item-image_wrapper">
-            <div className="menu__item-image_inner">
-              <img className="menu__item-image" src="assets/img_02.png" />
-            </div>
-          </div>
-          <span className="menu__item-text">
-            <span className="menu__item-innertext">&nbsp;Rubber</span>
-          </span>
-        </div>
         <br />
 
         <div className="menu__item">
@@ -94,19 +84,10 @@ export default function Projects() {
             </div>
           </div>
           <span className="menu__item-text">
-            <span className="menu__item-innertext">PU /</span>
+            <span className="menu__item-innertext">Wanted PreOnboarding</span>
           </span>
         </div>
-        <div className="menu__item">
-          <div className="menu__item-image_wrapper">
-            <div className="menu__item-image_inner">
-              <img className="menu__item-image" src="assets/img_04.png" />
-            </div>
-          </div>
-          <span className="menu__item-text">
-            <span className="menu__item-innertext">&nbsp;Technopolymers</span>
-          </span>
-        </div>
+
         <br />
         <div className="menu__item">
           <div className="menu__item-image_wrapper">
@@ -115,27 +96,19 @@ export default function Projects() {
             </div>
           </div>
           <span className="menu__item-text">
-            <span className="menu__item-innertext">Relife /</span>
+            <span className="menu__item-innertext">Fanagement </span>
           </span>
         </div>
+
+        <br />
         <div className="menu__item">
           <div className="menu__item-image_wrapper">
             <div className="menu__item-image_inner">
-              <img className="menu__item-image" src="assets/img_06.png" />
+              <img className="menu__item-image" src="assets/img_05.jpeg" />
             </div>
           </div>
           <span className="menu__item-text">
-            <span className="menu__item-innertext">&nbsp;TPU /</span>
-          </span>
-        </div>
-        <div className="menu__item">
-          <div className="menu__item-image_wrapper">
-            <div className="menu__item-image_inner">
-              <img className="menu__item-image" src="assets/img_07.png" />
-            </div>
-          </div>
-          <span className="menu__item-text">
-            <span className="menu__item-innertext">&nbsp;EVA</span>
+            <span className="menu__item-innertext">Bootcamp </span>
           </span>
         </div>
       </nav>
