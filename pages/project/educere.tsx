7e9b9f8f8dea6c5e64e9a8799a531539a80a6d97
Julie Lee <leejulie09@gmail.com>
2023-06-30 19:33:25 +0900

    Modify: images to project

---
+++ pages/project/educere.tsx
@@ -0,0 +1,22 @@
+export default function Educere() {
+  return (
+    <>
+      <div className="img-gallery" data-scroll-container>
+        <div className="img-gallery-container" data-scroll>
+          <div className="img">
+            <img src="/assets/educere_board.png" alt="fanddle" />
+          </div>
+          <div className="img">
+            <img src="/assets/educere/educere_board_cal.png" alt="wanted" />
+          </div>
+          <div className="img">
+            <img src="/assets/educere/educere_board_qna.png" alt="wanted" />
+          </div>
+          <div className="img">
+            <img src="/assets/educere/educere_board_write.png" alt="justcode" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/project/training.tsx
+++ pages/project/training.tsx
@@ -1,69 +1,22 @@
-import { useLayoutEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { Flip } from "gsap/Flip";
-
 export default function Training() {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    if (typeof document !== "undefined") {
-      // gsap.registerPlugin(Flip);
-      // CustomEase.create("cubic", "0.83, 0, 0.17, 1");
-      const gallery = document.querySelector(".img-gallery-container"),
-        images = gsap.utils.toArray(".img");
-      document.querySelector(".btn").addEventListener("click", () => {
-        // let state = Flip.getState(".img-gallery-container, .img");
-        gallery.classList.toggle("order");
-        images.forEach((wr, i) => {
-          wr.classList.toggle("reorder");
-        });
-        // Flip.from(state, {
-        //   absolute: true,
-        //   duration: 2,
-        //   stagger: 0.05,
-        //   ease: "cubic",
-        // });
-      });
-    }
-  }, []);
   return (
     <>
-      <div ref={ref} className="img-gallery" data-scroll-container>
+      <div className="img-gallery" data-scroll-container>
         <div className="img-gallery-container" data-scroll>
           <div className="img">
-            <img src="assets/fanddle_mypage.png" alt="fanddle" />
+            <img src="/assets/fanddle_mypage.png" alt="fanddle" />
           </div>
           <div className="img">
-            <img src="./images/img-2.png" />
+            <img src="/assets/training/wanted1.png" alt="wanted" />
           </div>
           <div className="img">
-            <img src="./images/img-10.png" />
+            <img src="/assets/training/wanted_chart.png" alt="wanted" />
           </div>
           <div className="img">
-            <img src="./images/img-4.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-5.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-6.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-7.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-8.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-9.png" />
-          </div>
-          <div className="img">
-            <img src="./images/img-2.png" />
+            <img src="/assets/training/justcode_clone.png" alt="justcode" />
           </div>
         </div>
       </div>
-
-      <div className="btn">Flip layout</div>
     </>
   );
 }
